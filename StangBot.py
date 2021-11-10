36e784ba3d22a46c9d6c0868a6a0dc34f44efeea
LastLife376 <halorocker18@gmail.com>
2021-11-09 20:56:08 -0500

    added ping

--- StangBot.py
+++ StangBot.py
@@ -8,4 +8,8 @@ TOKEN = 'OTA3NzgzMzY0NjAwMjcwODU4.YYsNIw.0-Aawrkj1aQwtJvgf_Pe-153kN0'
 async def on_ready():
     print('Bot is ready.')
 
+@client.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
 client.run(TOKEN)
