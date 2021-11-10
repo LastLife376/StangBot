681ee10b6cb821fb8b2884146109b9c79fa53f0e
LastLife376 <halorocker18@gmail.com>
2021-11-10 08:04:08 -0500

    added commands

--- StangBot.py
+++ StangBot.py
@@ -12,4 +12,16 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send('Pong!')
 
+@client.command()
+async def roster(ctx):
+    await ctx.send('Work in progress.')
+
+@client.command()
+async def games(ctx):
+    await ctx.send('Work in progress.')
+
+@client.command()
+async def practices(ctx):
+    await ctx.send('Work in progress.')
+
 client.run(TOKEN)
