4032b222c11a397cce9caad38846636e9a2bed74
LastLife376 <halorocker18@gmail.com>
2021-11-09 20:45:54 -0500

    first commit

---
+++ StangBot.py
@@ -0,0 +1,11 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '-')
+TOKEN = 'OTA3NzgzMzY0NjAwMjcwODU4.YYsNIw.0-Aawrkj1aQwtJvgf_Pe-153kN0'
+
+@client.event
+async def on_ready():
+    print('Bot is ready.')
+
+client.run(TOKEN)
